d761887a864a40f36d33c1d35096558e1d639009
Bappidiu <bappi15-4900@diu.edu.bd>
2024-11-19 09:34:51 +0600

    Working

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -62,110 +62,111 @@ const Home = () => {
         </div>
       </div>
 
-      <h1 className="text-center text-2xl sm:text-3xl font-bold text-blue-800 dark:text-white mt-6">
-        *Our specialists in different fields*
-      </h1>
-      <p className="container mx-auto px-4 w-full sm:w-[800px] text-center text-gray-600 dark:text-gray-400">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
-        facilis nisi nostrum praesentium vel, odio totam molestiae qui sint
-        suscipit architecto veniam consectetur? Iure dolore recusandae ut error.
-        Tempora, nostrum?
-      </p>
-
-      <div className="flex flex-wrap container mx-auto flex items-center justify-center">
-        {heroData.map((hero) => (
-          <div
-            key={hero.id}
-            className="w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 m-6"
-          >
+      <div>
+        <div className="text-[#333333] text-[32px] font-bold font-['Poppins'] text-center mt-10">
+          Browse The Range
+        </div>
+        <div className=" text-center text-[#666666] text-xl font-normal font-['Poppins'] ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <div className="container mx-auto lg:flex md:flex justify-center items-center gap-2.5 mt-10 ">
+          <div className=" hidden lg:block">
             <img
-              className="object-cover object-center w-full h-56"
-              src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-              alt="avatar"
+              className="mt-5 w-[372px] h-[480px] "
+              src="/src/assets/browse3.jpeg"
+              alt=""
             />
-            <div className="flex items-center px-6 py-3 bg-gray-900">
-              <svg
-                aria-label="headphones icon"
-                className="w-6 h-6 text-white fill-current"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M17 21C15.8954 21 15 20.1046 15 19V15C15 13.8954 15.8954 13 17 13H19V12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12V13H7C8.10457 13 9 13.8954 9 15V19C9 20.1046 8.10457 21 7 21H3V12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12V21H17ZM19 15H17V19H19V15ZM7 15H5V19H7V15Z"
-                />
-              </svg>
-              <h1 className="mx-3 text-lg font-semibold text-white">
-                {hero.title}
-              </h1>
-            </div>
-            <div className="px-6 py-4">
-              <h1 className="text-xl font-semibold text-gray-800 dark:text-white">
-                {hero.Name}
-              </h1>
-              <p className="py-2 text-gray-700 dark:text-gray-400">
-                {hero.description}
-              </p>
-              <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
-                <svg
-                  aria-label="suitcase icon"
-                  className="w-6 h-6 fill-current"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M14 11H10V13H14V11Z" />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M7 5V4C7 2.89545 7.89539 2 9 2H15C16.1046 2 17 2.89545 17 4V5H20C21.6569 5 23 6.34314 23 8V18C23 19.6569 21.6569 21 20 21H4C2.34314 21 1 19.6569 1 18V8C1 6.34314 2.34314 5 4 5H7ZM9 4H15V5H9V4ZM4 7C3.44775 7 3 7.44769 3 8V14H21V8C21 7.44769 20.5522 7 20 7H4ZM3 18V16H21V18C21 18.5523 20.5522 19 20 19H4C3.44775 19 3 18.5523 3 18Z"
-                  />
-                </svg>
-                <h1 className="px-2 text-sm">Meraki UI</h1>
-              </div>
-              <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
-                <svg
-                  aria-label="location pin icon"
-                  className="w-6 h-6 fill-current"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M16.2721 10.2721C16.2721 12.4813 14.4813 14.2721 12.2721 14.2721C10.063 14.2721 8.27214 12.4813 8.27214 10.2721C8.27214 8.063 10.063 6.27214 12.2721 6.27214C14.4813 6.27214 16.2721 8.063 16.2721 10.2721ZM14.2721 10.2721C14.2721 11.3767 13.3767 12.2721 12.2721 12.2721C11.1676 12.2721 10.2721 11.3767 10.2721 10.2721C10.2721 9.16757 11.1676 8.27214 12.2721 8.27214C13.3767 8.27214 14.2721 9.16757 14.2721 10.2721Z"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M5.79417 16.5183C2.19424 13.0909 2.05438 7.3941 5.48178 3.79418C8.90918 0.194258 14.6059 0.0543983 18.2059 3.48179C21.8058 6.90919 21.9457 12.606 18.5183 16.2059L12.3124 22.7241L5.79417 16.5183ZM17.0698 14.8268L12.243 19.8965L7.17324 15.0698C4.3733 12.404 4.26452 7.9732 6.93028 5.17326C9.59603 2.37332 14.0268 2.26454 16.8268 4.93029C19.6267 7.59604 19.7355 12.0269 17.0698 14.8268Z"
-                  />
-                </svg>
-                <h1 className="px-2 text-sm">California</h1>
-              </div>
-              <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
-                <svg
-                  aria-label="email icon"
-                  className="w-6 h-6 fill-current"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M3.00977 5.83789C3.00977 5.28561 3.45748 4.83789 4.00977 4.83789H20C20.5523 4.83789 21 5.28561 21 5.83789V17.1621C21 18.2667 20.1046 19.1621 19 19.1621H5C3.89543 19.1621 3 18.2667 3 17.1621V6.16211C3 6.11449 3.00333 6.06765 3.00977 6.0218V5.83789ZM5 8.06165V17.1621H19V8.06199L14.1215 12.9405C12.9499 14.1121 11.0504 14.1121 9.87885 12.9405L5 8.06165ZM6.57232 6.80554H17.428L12.7073 11.5263C12.3168 11.9168 11.6836 11.9168 11.2931 11.5263L6.57232 6.80554Z"
-                  />
-                </svg>
-                <h1 className="px-2 text-sm">patterson@example.com</h1>
-              </div>
-            </div>
+            <p className="text-center text-[#333333] text-2xl font-semibold font-['Poppins'] mt-4">
+              Living
+            </p>
           </div>
-        ))}
+          <div className="">
+            <img className=" mt-5" src="/src/assets/browse.png" alt="" />
+            <p className="text-center text-[#333333] text-2xl font-semibold font-['Poppins'] mt-4">
+              Dining
+            </p>
+          </div>
+          <div className="">
+            <img className="mt-5" src="/src/assets/browse2.png" alt="" />
+            <p className="text-center text-[#333333] text-2xl font-semibold font-['Poppins'] mt-4">
+              Bedroom
+            </p>
+          </div>
+        </div>
       </div>
+
+      <div className="text-center text-[#3a3a3a] text-[40px] font-bold font-['Poppins'] leading-[48px] mt-[56px]">
+        Our Products
+      </div>
+
+      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 items-center justify-center">
+  {heroData.slice(0, 8).map((hero) => (
+    <div
+      key={hero.id}
+      className="container mx-auto w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 m-6 relative group"
+    >
+      {/* The entire card including image and content */}
+      <div className="relative group w-full h-full">
+        {/* Card Image */}
+        <img
+          className="object-cover object-center w-full h-56 transition-all duration-300 group-hover:opacity-50"
+          src={hero.image}
+          alt="avatar"
+        />
+        {/* Card Content */}
+        <div className="container mx-auto p-6 group-hover:opacity-0 transition-opacity duration-300">
+          <div className="text-[#3a3a3a] text-2xl font-semibold font-['Poppins'] leading-[28.80px]">{hero.title}</div>
+          <div className="text-[#898989] text-base font-medium font-['Poppins'] leading-normal">{hero.category}</div>
+          <div className="flex">
+            <div className="text-[#3a3a3a] text-xl font-semibold font-['Poppins'] leading-[30px]">Rp {hero.price}</div>
+            <div className="text-[#b0b0b0] text-base font-normal font-['Poppins'] line-through leading-normal ml-6">Rp {hero.old_price}</div>
+          </div>
+        </div>
+
+        {/* Discount Badge */}
+        <div className="w-12 h-12 left-[213px] top-[24px] absolute">
+          <div className="w-12 h-12 left-0 top-0 absolute bg-[#e97171] rounded-full" />
+          <div className="left-[5px] top-[12px] absolute text-white text-base font-medium font-['Poppins'] leading-normal">
+            -{hero.discount_percent}%
+          </div>
+        </div>
+
+        {/* Overlay for Background Blur */}
+        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-blur-sm ">
+          <div className="flex mt-[247px]">
+          <div className="flex w-4 h-4 relative ml-[20px]">
+          <img src="/src/assets/gridicons_share.svg" alt="" />
+          <div className="text-white text-base font-semibold font-['Poppins'] leading-normal">Share</div>
+          </div>
+          <div className="flex w-4 h-4 relative ml-[100px]">
+          <img src="/src/assets/compare-svgrepo-com 1.svg" alt="" />
+          <div className="text-white text-base font-semibold font-['Poppins'] leading-normal">Compare</div>
+          </div>
+          <div className="flex w-4 h-4 relative ml-[120px]">
+          <img src="/src/assets/Heart.svg" alt="" />
+          <div className="text-white text-base font-semibold font-['Poppins'] leading-normal">Like</div>
+          </div>
+          </div>
+        </div>
+
+        {/* Button Overlay on Hover */}
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+
+         <button>
+         <div className="w-[202px] h-12 bg-white">
+          <div className="text-[#b88e2f] text-base font-semibold font-['Poppins'] leading-normal pt-3">Add to cart</div>
+          </div>
+         </button>
+        </div>
+        
+      </div>
+    </div>
+  ))}
+</div>
+
+
+
+
     </div>
   );
 };
