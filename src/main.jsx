c3d33204036e80d826257f56067e7605cc4fca38
Bappidiu <bappi15-4900@diu.edu.bd>
2024-11-24 23:30:24 +0600

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import './index.css'
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     
     <RouterProvider router={router} />
-  </StrictMode>,
+  </React.StrictMode>,
 )
